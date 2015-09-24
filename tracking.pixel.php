d2eb75e4d62d1916bd5b1096456733c03d7cf700
ershov-ilya <ershov.ilya@gmail.com>
2015-09-24 10:06:57 +0300

    DEBUG mode

--- tracking.pixel.php
+++ tracking.pixel.php
@@ -9,11 +9,20 @@
  * Time: 9:58
  */
 
+if(isset($_GET['t'])) define('DEBUG', true);
+defined('DEBUG') or define('DEBUG', false);
+
+if(DEBUG){
+    header("Content-type: text/plain");
+//    print_r($_SERVER);
+    die;
+}
+
 header("content-type: image/gif");
 setcookie('test',rand(),0,'/');
+print base64_decode("R0lGODlhAQABAIAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==");
 
 //$s=date("Y-m-d H:i:s") . ": ". $_SERVER['REMOTE_ADDR'] . " : " .$_GET['id']. "\n";
 //file_put_contents("my_track_file.txt", $s, FILE_APPEND);
 
 
-die(base64_decode("R0lGODlhAQABAIAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="));
